7057f77681d44d37fce011771b4d1ce25f6f1e51
Isai <isakoospike@gmail.com>
2023-09-13 08:48:30 -0500

    elimanado la validacion de imagen que funciona mal

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,47 +1,32 @@
-  import React from "react";
-  import { Link } from "react-router-dom";
-  import style from "./Card.module.css";
-  import silueta from '../../images/silueta.webp'
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./Card.module.css";
 
-  const Card = ({ image, name, types }) => {
+const Card = ({ image, name, types }) => {
+  return (
+    <Link to={`/detail/${name}`} className={style.cardContainer}>
+      <div className={style.name}>
+        <h2>{name}</h2>
+      </div>
 
-    const isImageValid = (url) => {
-      const img = new Image();
-      img.src = url;
-      return img.complete && img.naturalWidth > 0;
-    };
-
-    const isValidImage = isImageValid(image);
-
-    return (
-      <Link to={`/detail/${name}`} className={style.cardContainer}>
-        <div className={style.name}>
-          <h2>{name}</h2>
-        </div>
-
-        <div>
-        {isValidImage ? (
-            <img className={style.imagePokemon} src={image} alt={name} />
-          ) : (
-            <img className={style.imagePokemon} src={silueta} alt="Silueta" />
-          )}
-        </div>
-        <div className={style.typesContainer}>
-          {types?.map((type) => (
-            <div key={type.name}>
-              <div className={style.typesDiv}>
-                <span
-                  className={`${style[type.name]} ${style.types}`}
-                ></span>
-              </div>
-                <p className={style.nameType}>{type.name}</p>
+      <div>
+        <img className={style.imagePokemon} src={image} alt={name} />
+      </div>
+      <div className={style.typesContainer}>
+        {types?.map((type) => (
+          <div key={type.name}>
+            <div className={style.typesDiv}>
+              <span
+                className={`${style[type.name]} ${style.types}`}
+              ></span>
             </div>
-          ))}
-        </div>
-      </Link>
-    );
-  };
+              <p className={style.nameType}>{type.name}</p>
+          </div>
+        ))}
+      </div>
+    </Link>
+  );
+};
 
 
-  export default Card;
-
+export default Card;
