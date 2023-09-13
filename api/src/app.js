b88c5a609e75b76d7dc3a0c2c2472eb1391e62d6
Isai <isakoospike@gmail.com>
2023-09-13 08:23:24 -0500

    cambio header en el backend para aceptar peticiones de localhost

--- api/src/app.js
+++ api/src/app.js
@@ -16,7 +16,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173/'); // update to match the domain you will make the request from https://pi-pokemon-isai.vercel.app/
+  res.header('Access-Control-Allow-Origin', 'https://pi-pokemon-isai.vercel.app/'); // update to match the domain you will make the request from https://pi-pokemon-isai.vercel.app/
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -25,7 +25,7 @@ server.use((req, res, next) => {
 
 
 const corsOptions = {
-  origin: 'http://localhost:5731/', 
+  origin: 'https://pi-pokemon-isai.vercel.app/', 
   credentials: true, 
 };
 
